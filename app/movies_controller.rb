6fea08b98c8c10472e902cfadb812ed6a56a6dd9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 22:16:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -40,7 +40,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.group(:id).count
+  Movie.group(:id).count.size
 end
 
 def can_find_the_first_item_from_the_database_using_id

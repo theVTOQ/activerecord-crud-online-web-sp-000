8466cfb5e999334d52fdb670efc7c98db9198b18
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 22:19:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,7 +44,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 0)
+  #Movie.find_by(id: 0)
+  Movie.all
 end
 
 def can_find_by_multiple_attributes

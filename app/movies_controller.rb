5ac69931b0f276fb590e9360744dde1ede3f94f9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 22:24:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,7 +74,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.find_by(title: "Wat?")
-  movie.update(title: "Even Awesomer Flick")
+  movie.update(title: "Wat, huh?")
   movie.save
 end
 

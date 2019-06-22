bcdeeaffb7015195bd5139a64123d02d6fbdbd56
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 22:25:17 +0000

    Done.

---
+++ misc/migrate/001_create_movies.rb
@@ -0,0 +1,9 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  create_table :movies do |t|
+    t.string :title
+    t.datetime :release_date
+    t.string :director
+    t.string :lead
+    t.boolean :in_theaters
+  end
+end
